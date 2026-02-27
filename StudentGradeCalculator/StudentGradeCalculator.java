b7813782d3c400e786d7dc1ab9d54b89038af602
sneha1345 <snehamore48946@gmail.com>
2026-02-27 23:39:30 +0530

    Added StudentGradeCalculator

---
+++ StudentGradeCalculator/StudentGradeCalculator.java
@@ -0,0 +1,57 @@
+import java.util.Scanner;
+
+public class StudentGradeCalculator {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("------ Student Grade Calculator ------");
+
+        System.out.print("Enter number of subjects: ");
+        int subjects = sc.nextInt();
+
+        int total = 0;
+
+        for (int i = 1; i <= subjects; i++) {
+
+            System.out.print("Enter marks for subject " + i + " (out of 100): ");
+            int marks = sc.nextInt();
+
+            if (marks < 0 || marks > 100) {
+                System.out.println("Invalid marks! Please enter between 0 and 100...!!!");
+                i--;  
+                continue;
+            }
+
+            total += marks;
+        }
+
+        double average = (double) total / subjects;
+
+        char grade;
+
+        if (average >= 90) {
+            grade = 'A';
+        }
+        else if (average >= 80) {
+            grade = 'B';
+        }
+        else if (average >= 70) {
+            grade = 'C';
+        }
+        else if (average >= 60) {
+            grade = 'D';
+        }
+        else {
+            grade = 'F';
+        }
+
+        System.out.println("\n------ Result ------");
+        System.out.println("Total Marks: " + total);
+        System.out.println("Average Percentage: " + average + "%");
+        System.out.println("Grade: " + grade);
+
+        sc.close();
+    }
+}
